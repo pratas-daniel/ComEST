2d97e78a882e312138ddbad87adb657d6a587d24
Pratezz <kikoprates@hotmail.com>
2022-04-23 17:32:03 +0100

    update contrucs v2

--- src/sistema/Pedido.java
+++ src/sistema/Pedido.java
@@ -10,13 +10,17 @@ import restaurante.*;
  * ao qual o pedido está associado.
  */
 public class Pedido {
+	private String id;
 	private Restaurante restaurante;
 	private ArrayList<Escolha> escolhas;
 
-	public Pedido(Restaurante restaurante, ArrayList<Escolha> escolhas) {
+
+	public Pedido(String id, Restaurante restaurante, ArrayList<Escolha> escolhas) {
+		this.id = id;
 		this.restaurante = restaurante;
 		this.escolhas = escolhas;
 	}
+
 
 	/** indica se o pedido não tem escolhas associadas
 	 * @return true, se o pedido não tiver qualquer escolha
